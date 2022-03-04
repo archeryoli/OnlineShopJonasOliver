3066f6d327d1299834dc0864ceaccd142cc289dc
archeryoli <oliver.brandstetter2506@gmail.com>
2022-03-04 13:17:16 +0100

    Added Lombok Library and added part of Article Class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import Models.Article;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Article article = new Article();
+        article.setProductId(1);
+        System.out.println(article.getProductId());
     }
 }
