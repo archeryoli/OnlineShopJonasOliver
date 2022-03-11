054081daaf6c699a94998f3aca563ab885e6dd10
archeryoli <oliver.brandstetter2506@gmail.com>
2022-03-11 13:06:24 +0100

    Clothing Klasse hinzugefügt, sowie ClothingType-Enum. Auch im Main instanz hinzugefügt

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import Models.Clothing;
+import Models.ClothingType;
 import Models.Electronics;
 
 public class Main {
     public static void main(String[] args) {
         Electronics e = new Electronics(0, "Waschmaschine", 239.99, "Bosch", "Tolle Waschmashcine", 15, 5, "Model 1", 1350, "50x50x50cm");
+        Clothing c = new Clothing(1, "Jean test", 60.50, "Tommy Jeans", "Tolle Jeans zum Tragen für die Arbeit", 0.2, 3, ClothingType.JEANS, "Schwarz", 35, "Baumwolle");
         System.out.println(e);
+        System.out.println(c);
     }
 }
